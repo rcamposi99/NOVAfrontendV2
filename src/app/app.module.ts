b94e665f757f3e5b21fed520703603e4b7d5ac47
rcamposi99 <roger.camposisans@everis.nttdata.com>
2022-04-21 12:23:32 +0200

    Modulos de material importados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { AnyadirTareaComponent } from './anyadir-tarea/anyadir-tarea.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { AnyadirTareaComponent } from './anyadir-tarea/anyadir-tarea.component';
     HttpClientModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [TareasServiceService],
   bootstrap: [AppComponent]
