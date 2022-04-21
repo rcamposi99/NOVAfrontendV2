e8e880cbd1dc88658b375ae06633f9ccb8b88a5f
rcamposi99 <roger.camposisans@everis.nttdata.com>
2022-04-21 14:54:48 +0200

    formulario add task creado

--- src/app/anyadir-tarea/anyadir-tarea.component.ts
+++ src/app/anyadir-tarea/anyadir-tarea.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Tarea } from '../services/tareasService/tarea';
 
 @Component({
   selector: 'app-anyadir-tarea',
   templateUrl: './anyadir-tarea.component.html',
   styleUrls: ['./anyadir-tarea.component.scss']
 })
+
 export class AnyadirTareaComponent implements OnInit {
+
+  tarea1:Tarea = {};
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public tareaCreada():void{
+
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { AnyadirTareaComponent } from './anyadir-tarea/anyadir-tarea.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -26,8 +28,10 @@ import {MatSelectModule} from '@angular/material/select';
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
-    MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    FormsModule,
+    MatDividerModule,
+    MatIconModule
   ],
   providers: [TareasServiceService],
   bootstrap: [AppComponent]
